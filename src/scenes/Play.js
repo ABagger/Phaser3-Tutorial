6c9513171d7aa86a6555fc9fa1dde7ec482a2b70
Andrew Bagger <andrewbagger@gmail.com>
2021-09-18 17:12:39 -0700

    Added assets, implemented screen scrolling

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -5,15 +5,32 @@ class Play extends Phaser.Scene
         super('playScene')
     }
 
+    preload()
+    {
+    // preloading assets
+    this.load.image('rocket', './assets/rocket.png');
+    this.load.image('spaceship', './assets/spaceship.png');
+    this.load.image('starfield', './assets/starfield.png');
+    }
+
+
     create()
     {
+        // implementing starfield scroll
+        this.starfield = this.add.tileSprite(0, 0, 640, 480, 'starfield').setOrigin(0, 0);
+        
         // green UI background
         this.add.rectangle(0, borderUIsize + borderPadding, game.config.width, borderUIsize * 2, 0x00FF00).setOrigin(0,0);
         
         // white canvas borders
         this.add.rectangle(0, 0, game.config.width, borderUIsize, 0xFFFFFF).setOrigin(0, 0);
         this.add.rectangle(0, game.config.height - borderUIsize, game.config.width, borderUIsize, 0xFFFFFF).setOrigin(0,0);
-        this.add.rectangle(0, 0, borderUIsize, game.config.height, 0xFFFFF).setOrigin(0, 0);
+        this.add.rectangle(0, 0, borderUIsize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
         this.add.rectangle(game.config.width - borderUIsize, 0, borderUIsize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
+    }
+
+    update()
+    {
+        this.starfield.tilePositionX -= 1;
     }
 }
