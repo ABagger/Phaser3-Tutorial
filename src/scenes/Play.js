95a8e2b209292b651a93fb3a2368f7ed2f39ee80
Andrew Bagger <andrewbagger@gmail.com>
2021-09-18 18:02:45 -0700

    Added spaceships

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -31,6 +31,11 @@ class Play extends Phaser.Scene
         // adding rocket (p1)
         this.p1Rocket = new Rocket(this, game.config.width/2, game.config.height - borderUIsize - borderPadding, 'rocket').setOrigin(.5, 0);
     
+        // adding spaceships x3
+        this.spaceship01 = new Spaceship(this, game.config.width + borderUIsize * 6, borderUIsize * 4, 'spaceship', 0, 30).setOrigin(0, 0);
+        this.spaceship02 = new Spaceship(this, game.config.width + borderUIsize * 3, borderUIsize * 5 + borderPadding * 2, 'spaceship', 0, 20).setOrigin(0, 0);
+        this.spaceship03 = new Spaceship(this, game.config.width, borderUIsize * 6 + borderPadding * 4, 'spaceship', 0, 10).setOrigin(0, 0);
+
         keyF = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
         keyR = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
         keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
@@ -41,5 +46,8 @@ class Play extends Phaser.Scene
     {
         this.starfield.tilePositionX -= 1;
         this.p1Rocket.update();
+        this.spaceship01.update();
+        this.spaceship02.update();
+        this.spaceship03.update();
     }
 }
