f354bd68a00bdf21540df0226f3975d7c69a237e
Andrew Bagger <andrewbagger@gmail.com>
2021-09-22 11:01:16 -0700

    Implemented basic start menu Resized title sprite, added button to start game

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -5,9 +5,30 @@ class Menu extends Phaser.Scene
         super('menuScene')
     }
 
+    preload()
+    {
+        this.load.image('title', './assets/title.png');
+    }
+
     create()
     {
-        this.add.text(320, 240, 'Menu: ').setOrigin(0.5, 0.5);
-        this.scene.start('playScene');
+        let menuConfig = {
+            fontFamily : 'Courier', // 'bold',
+            fontSize : '18px',
+            //backgroundColor : '#ffffff',
+            color : '#dcffce',
+            align : 'center',
+        }
+
+        this.add.text(320, 260, 'Press F to begin', menuConfig).setOrigin(.5, 0);
+        this.add.image(320, 260, 'title').setOrigin(.5, 1);
+
+        keyF = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
+    }
+
+    update()
+    {
+        if(Phaser.Input.Keyboard.JustDown(keyF))
+            this.scene.start('playScene');
     }
 }
